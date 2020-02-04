077add7ce7531d40c5fc204167f9491c9500c4d2
Felix <felixbeuving@live.nl>
2020-02-04 15:14:17 +0100

    init commit
    SteeringCS van elo

---
+++ AAI-assignment/behaviour/SeekBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using AAI_assignment.entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAI_assignment.behaviour
+{
+    class SeekBehaviour 
+    {
+        // to do
+    }
+}
---
+++ AAI-assignment/behaviour/SteeringBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using AAI_assignment.entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAI_assignment
+{
+    abstract class SteeringBehaviour
+    {
+        public MovingEntity ME { get; set; }
+        public abstract Vector2D Calculate();
+
+        public SteeringBehaviour(MovingEntity me)
+        {
+            ME = me;
+        }
+    }
+
+    
+}
